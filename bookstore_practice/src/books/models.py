c50df08d766702f4324c69d55592f8cd8f254be2
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-05-06 18:50:07 -0400

    add field for author_name to book, add field pic to book, alter field book_type. alter field genre

--- bookstore_practice/src/books/models.py
+++ bookstore_practice/src/books/models.py
@@ -3,25 +3,27 @@ from django.db import models
 # Genre choices
 
 genre_choices = (
-    ('classic', 'Classing'),
-    ('romantic', 'Romantic'),
-    ('comic', 'Comic'),
-    ('fantasy', 'Fantasy'),
-    ('horror', 'Horror'),
-    ('educational', 'Educational')
+    ('cl', 'Classing'),
+    ('ro', 'Romantic'),
+    ('co', 'Comic'),
+    ('fa', 'Fantasy'),
+    ('ho', 'Horror'),
+    ('ed', 'Educational')
 )
 
 book_type_choices = (
-    ('hardcover', 'Hard cover'),
-    ('ebook', 'E-Book'),
-    ('audiobook', 'Audiobook')
+    ('hc', 'Hard cover'),
+    ('eb', 'E-Book'),
+    ('ab', 'Audiobook')
 )
 
 class Book (models.Model):
     name = models.CharField(max_length=120)
+    author_name = models.CharField(max_length=120, default='Unknown Author')
     price = models.FloatField(help_text='in US dollars $')
     genre = models.CharField(max_length=12, choices=genre_choices, default='cl')
     book_type = models.CharField(max_length=12, choices=book_type_choices, default='hc')
+    pic = models.ImageField(upload_to='books', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
